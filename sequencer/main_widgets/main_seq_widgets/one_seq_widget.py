d2ed9dd8957f0098d0ee91afa999f47674445c70
Mahmoud_Badawy <mahmoud1816yasser@gmail.com>
2024-07-10 16:00:26 -0400

    a

--- sequencer/main_widgets/main_seq_widgets/one_seq_widget.py
+++ sequencer/main_widgets/main_seq_widgets/one_seq_widget.py
@@ -407,6 +407,8 @@ class EventButton(QPushButton):
                 # associated_parameters is a list 
                 for p in self.event.associated_parameters:
                     behavior += f"\n{p.name}: {p.get_value()}"
+            behavior += f"comment: {self.event.comment}"
+
             QToolTip.showText(event.globalPos(), behavior, self)
             super().enterEvent(event)
  
